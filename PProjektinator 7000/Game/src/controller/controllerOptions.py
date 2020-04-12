eaca62840521a813d1b44a9e5bc2b77123f6f631
anmach <ania.m10110@gmail.com>
2020-04-12 20:51:27 +0200

    Slider
    Jeździ, działa, jest supcio

--- PProjektinator 7000/Game/src/controller/controllerOptions.py
+++ PProjektinator 7000/Game/src/controller/controllerOptions.py
@@ -8,32 +8,49 @@ class ControllerOptions(Controller):
   #przetwarzanie danych wejściowych
     def __init__(self):
         super().__init__()
+
+        self._is_mouse_pressed = 0
         
         # tablica sliderów
         self._sliders = []
 
     def process_input(self):
-        for event in py.event.get():
+        if self._is_mouse_pressed == 0:
+            for event in py.event.get():
+                #naciśnięcie X okna
+                if event.type == py.QUIT:
+                    self._command = Command.EXIT
 
-            #naciśnięcie X okna
-            if event.type == py.QUIT:
-                self._command = Command.EXIT
-
-            #kliknięcie myszką
-            if event.type == py.MOUSEBUTTONDOWN:
-                for control in self._controls:
-                    #sprawdzanie czy nad daną kontrolką jest kursor
-                    if control.get_is_focused():
-                        self._command = control.get_command()
-                        break
-                for slider in self._sliders:
-                    if slider.get_is_focused():
-                        slider.move()
-                        break
-            else:
-                self._command = Command.CONTINUE
-            
-
+                # kliknięcie myszką
+                if event.type == py.MOUSEBUTTONDOWN:
+                    for control in self._controls:
+                        # sprawdzanie czy nad daną kontrolką jest kursor
+                        if control.get_is_focused():
+                            self._command = control.get_command()
+                            break
+                    for slider in self._sliders:
+                        #sprawdzenie czy nad sliderem jest kursor
+                        if slider.get_is_focused():
+                            self._is_mouse_pressed = 1
+                            slider.set_is_pushed(1)
+                            slider.move()
+                            break
+                else:
+                    self._command = Command.CONTINUE
+        else:    
+            # poruszanie slidera
+            for slider in self._sliders:
+                if slider.get_is_focused():
+                    slider.move()
+                    break 
+            for event in py.event.get():
+                # koniec nacisku myszy
+                if event.type == py.MOUSEBUTTONUP:
+                    self._is_mouse_pressed = 0
+                    # zresetowanie sliderów jako nienaciśnięte
+                    for slider in self._sliders:
+                        slider.set_is_pushed(0)
+               
     #w menu nie ma potrzeby przekazywania modelu do widoku
     def communicateMV(self, model, view):
         pass
