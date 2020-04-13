cd417dd8ac70b830e8abb45507479146f77583ff
Mateusz Chłopek <32665400+OftenDeadKanji@users.noreply.github.com>
2020-04-13 13:05:22 +0200

    13 kwi 2020
    - usunięcie adnotacji @abstractmethod
    - poprawa nazewnictwa zmiennych i metod
    - dodanie getterów

--- PProjektinator 7000/Game/src/model/modelLevelEditor.py
+++ PProjektinator 7000/Game/src/model/modelLevelEditor.py
@@ -12,47 +12,44 @@ class ModelLevelEditor(Model):
 
         #pobranie informacji o utworzonych już poziomach
         #       v-----inaczej nie działa-----v
-        for (_, _, fileList) in os.walk('.\\saves\\levels'):
-            self.__levelList = fileList
+        for (_, _, file_list) in os.walk('.\\saves\\levels'):
+            self.__level_list = file_list
 
         #nr aktualnie wybranego, edytowanego poziomu
-        self.__chosenLevel = -1
+        self.__chosen_level = -1
 
         #wyświetlany nr poziomu do edycji (zmieniany za pomocą przycisków)
-        self.__levelToEdit = 0
+        self.__level_to_edit = 0
 
         #współrzędne punktów nowej platformy
-        self.__newPlatformCoords = (-1, -1)
-
-        #liczba utworzonych już wierzchołków nowej platformy
-        self.__createdVerticesNumber = 0
+        self.__new_platform_coords = (-1, -1)
 
         #aktualny tryb pracy
         self.__mode = EditingMode.NONE
 
     #metoda aktualizująca stan wewnętrzego modelu programu
-    @abstractmethod
     def update(self):
         if self._command == Command.EXIT:
             self._runMode = False
 
-        elif self._command == Command.PREV_LEVEL and self.__levelToEdit > 0:
-            self.__levelToEdit -= 1
+        elif self._command == Command.PREV_LEVEL and self.__level_to_edit > 0:
+            self.__level_to_edit -= 1
 
-        elif self._command == Command.NEXT_LEVEL and self.__levelToEdit < (len(self.__levelList) - 1):
-            self.__levelToEdit += 1
+        elif self._command == Command.NEXT_LEVEL and self.__level_to_edit < (len(self.__level_list) - 1):
+            self.__level_to_edit += 1
 
-        elif self._command == Command.EDIT and len(self.__levelList) > 0:
-            self.__chosenLevel = self.__levelToEdit
+        elif self._command == Command.EDIT and len(self.__level_list) > 0:
+            self.__chosen_level = self.__level_to_edit
             #wczytanie nowego poziomu
 
         elif self._command == Command.CREATE_NEW:
             pass
             #utworzenie nowego poziomu i wczytanie go
 
-        elif self._command == Command.SAVE and self.__chosenLevel != -1:
+        elif self._command == Command.SAVE and self.__chosen_level != -1:
             pass
             #zapisanie aktualnie modyfikowanego poziomu
+
         elif self.__mode != EditingMode.NONE:
 
             #lewy przycisk myszki
@@ -64,12 +61,13 @@ class ModelLevelEditor(Model):
                     #sprawdzenie czy nie nachodzi/koliduje z innymi obiektami
                     #albo i bez tego, gdyż wtedy można [łatwiej] robić platformy, które nie są prostokątami
             
-                    if self.__newPlatformCoords == (-1, -1):
-                        self.__newPlatformCoords = py.mouse.get_pos()
+                    if self.__new_platform_coords == (-1, -1):
+                        self.__new_platform_coords = py.mouse.get_pos()
                     else:
                         #TODO
                         #dodanie nowej platformy o współrzędnych wierzchołków [tworzących przekątną] - (self.__newPlatformCoords, py.mouse.get_pos())
-                        pass
+
+                        self.__new_platform_coords = (-1, -1)
             
                 elif self.__mode == EditingMode.OBJECT_PLACEMENT:
                     #TODO
@@ -83,13 +81,23 @@ class ModelLevelEditor(Model):
                 if self.__mode == EditingMode.PLATFORM_CREATION:
 
                     #ale bez wierzchołków - wychodzimy z tego trybu
-                    if self.__newPlatformCoords == (-1, -1):
+                    if self.__new_platform_coords == (-1, -1):
                         self.__mode = EditingMode.NONE
 
                     #już z jednym wierzchołkiem - usuwamy go
                     else:
-                        self.__newPlatformCoords = (-1, -1)
+                        self.__new_platform_coords = (-1, -1)
 
                 #w trybie wstawianie nowego obiektu
                 elif self.__mode == EditingMode.OBJECT_PLACEMENT:
                     self.__mode == EditingMode.NONE
+
+    #v----GETTERY----v
+    def get_level_to_edit_number(self):
+        return self.__level_to_edit
+
+    def get_new_platform_coords(self):
+        return self.__new_platform_coords
+
+    def get_mode(self):
+        return self.__mode
