1526c81461a45e1bf87f1ec39559b6e391c25cba
Mateusz Chłopek <32665400+OftenDeadKanji@users.noreply.github.com>
2020-05-02 18:37:30 +0200

    2 maj 2020
    metody do zapisu obiektów

--- PProjektinator 7000/Game/src/view/Game/movingPlatform.py
+++ PProjektinator 7000/Game/src/view/Game/movingPlatform.py
@@ -20,3 +20,22 @@ class MovingPlatform(GameObject):
         if self.path_cur_y >= self.path_max_y or self.path_cur_y <= 0:
             self.spd_y = -self.spd_y
 
+    #metoda służąca do zapisywania aktualnego stanu obiektu do pliku
+    def saveToFile(self, file):
+        file.write('@<JAKIEŚ ID>')
+        file.write('#direction\n' + str(self.direction) + '\n')
+        file.write('#type\n' + str(self.type) + '\n')
+        file.write('#spd_x\n' + str(self.spd_x) + '\n')
+        file.write('#spd_x_other\n' + str(self.spd_x_other) + '\n')
+        file.write('#spd_y\n' + str(self.spd_y) + '\n')
+        file.write('#spd_y_other\n' + str(self.spd_y_other) + '\n')
+        file.write('#width\n' + str(self.width) + '\n')
+        file.write('#height\n' + str(self.height) + '\n')
+        file.write('#does_gravity\n' + str(self.does_gravity) + '\n')
+        #file.write('#surf\n' + str(self.surf) + '\n')
+        file.write('#frame_id\n' + str(self.frame_id) + '\n')
+        file.write('#path_max_x\n' + str(self.path_max_x) + '\n')
+        file.write('#path_max_y\n' + str(self.path_max_y) + '\n')
+        file.write('#path_cur_x\n' + str(self.path_cur_x) + '\n')
+        file.write('#path_cur_y\n' + str(self.path_cur_y) + '\n')
+
--- PProjektinator 7000/Game/src/view/Game/player.py
+++ PProjektinator 7000/Game/src/view/Game/player.py
@@ -29,5 +29,19 @@ class Player(GameObject):
         self.spd_x_other = 0
         self.spd_y_other = 0
 
-
+        #metoda służąca do zapisywania aktualnego stanu obiektu do pliku
+        def saveToFile(self, file):
+            file.write('@<JAKIEŚ ID>')
+            file.write('#direction\n' + str(self.direction) + '\n')
+            file.write('#type\n' + str(self.type) + '\n')
+            file.write('#spd_x\n' + str(self.spd_x) + '\n')
+            file.write('#spd_x_other\n' + str(self.spd_x_other) + '\n')
+            file.write('#spd_y\n' + str(self.spd_y) + '\n')
+            file.write('#spd_y_other\n' + str(self.spd_y_other) + '\n')
+            file.write('#width\n' + str(self.width) + '\n')
+            file.write('#height\n' + str(self.height) + '\n')
+            file.write('#does_gravity\n' + str(self.does_gravity) + '\n')
+            #file.write('#surf\n' + str(self.surf) + '\n')
+            file.write('#frame_id\n' + str(self.frame_id) + '\n')
+            file.write('#isWalking\n' + str(self.isWalking) + '\n')
 
