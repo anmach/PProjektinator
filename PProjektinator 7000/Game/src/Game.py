eeb66e806625723ae25a7133f449f678db2332c9
anmach <ania.m10110@gmail.com>
2020-05-11 03:06:14 +0200

    Odtwarzanie muzyki

--- PProjektinator 7000/Game/src/Game.py
+++ PProjektinator 7000/Game/src/Game.py
@@ -7,7 +7,16 @@ def main():
     result = py.init()
     if result[1] == 0:
         print("Inicjalizacja przebiegła pomyślnie.") 
-    
+
+        #muzyka
+        playlist = list()
+        playlist.append ( ".\\res\\music\\EpicTVTheme.mp3" )
+        playlist.append (".\\res\\music\\Assasins.mp3")
+
+        py.mixer.music.load ( playlist.pop() )  
+        py.mixer.music.queue ( playlist.pop() )
+        py.mixer.music.play(-1)
+            
         menu = Menu()
         menu.run()
     else:
