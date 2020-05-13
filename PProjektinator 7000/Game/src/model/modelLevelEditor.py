6592a4ae9aedaf2eaca203414be492dfe450dcfd
Mateusz Chłopek <32665400+OftenDeadKanji@users.noreply.github.com>
2020-05-13 15:03:06 +0200

    13 maj 2020
    Małe poprawki w edytorze.

--- PProjektinator 7000/Game/src/model/modelLevelEditor.py
+++ PProjektinator 7000/Game/src/model/modelLevelEditor.py
@@ -79,90 +79,113 @@ class ModelLevelEditor(Model):
 
         elif self._command == Command.CREATE_PLATFORM:
             self.__mode = EditingMode.PLATFORM_CREATION
+
+            self.__new_platform_first_vertex_pos = (-1, -1)
+            self.__new_platform_second_vertex_pos = (-1, -1)
+            self.__new_platform_vertex_number = 1
         
         elif self._command == Command.PLACE_PLAYER:
             self.__mode = EditingMode.OBJECT_PLACEMENT
 
-        #interpretacja akcji użytkownika zależy od trybu w znajduje się edytor
+            self.__new_platform_first_vertex_pos = (-1, -1)
+            self.__new_platform_second_vertex_pos = (-1, -1)
+            self.__new_platform_vertex_number = 1
+
+        #interpretacja akcji użytkownika na polu edycyjnym zależy od trybu w jakim znajduje się edytor
             
         #tworzenie nowej platformy
         if self.__mode == EditingMode.PLATFORM_CREATION:
-            #TODO
-            #sprawdzenie czy nie nachodzi/koliduje z innymi obiektami
+            update_platform_creation()
 
-            mouse_pos = py.mouse.get_pos()
-            new_vertex_pos = mouse_pos
+        #wstawianie nowego obiektu
+        elif self.__mode == EditingMode.OBJECT_PLACEMENT:
+            update_object_placement()
+        
+        #usuwanie obiektu (lub platformy)
+        elif self.__mode == EditingMode.DELETION:
+            update_deletion()
 
-            for game_object in self.__game_objects_arr:
-                    if type(game_object) is GameObject and game_object.get_type() == ObjectType.STATIC:
-                        x0 = game_object.get_x()
-                        x1 = game_object.get_x() + game_object.get_width()
-                        
-                        y0 = game_object.get_y()
-                        y1 = game_object.get_y() + game_object.get_height()
+    def update_platform_creation(self):
+        #TODO
+        #sprawdzenie czy nie nachodzi/koliduje z innymi obiektami
 
-                        #jeżeli różnica między pozycją kursora myszki, a wierzchołkiem istniejącej platformy jest mniejsza niż ustalona
-                        #wartość to nowy wierzchołek jest "przyczepiany" do już istniejącego
-                        if abs(mouse_pos[0] - x0) < self.__snap_distance and abs(mouse_pos[1] - y0) < self.__snap_distance:
-                            new_vertex_pos = (x0, y0)
-                        elif abs(mouse_pos[0] - x1) < self.__snap_distance and abs(mouse_pos[1] - y0) < self.__snap_distance:
-                            new_vertex_pos = (x1, y0)
-                        elif abs(mouse_pos[0] - x0) < self.__snap_distance and abs(mouse_pos[1] - y1) < self.__snap_distance:
-                            new_vertex_pos = (x0, y1)
-                        elif abs(mouse_pos[0] - x1) < self.__snap_distance and abs(mouse_pos[1] - y1) < self.__snap_distance:
-                            new_vertex_pos = (x1, y1)
+        mouse_pos = py.mouse.get_pos()
+        new_vertex_pos = mouse_pos
+
+        for game_object in self.__game_objects_arr:
+                if type(game_object) is GameObject and game_object.get_type() == ObjectType.STATIC:
+                    x0 = game_object.get_x()
+                    x1 = game_object.get_x() + game_object.get_width()
+                    
+                    y0 = game_object.get_y()
+                    y1 = game_object.get_y() + game_object.get_height()
+
+                    #jeżeli różnica między pozycją kursora myszki, a wierzchołkiem istniejącej platformy jest mniejsza niż ustalona
+                    #wartość to nowy wierzchołek jest "przyczepiany" do już istniejącego
+                    if abs(mouse_pos[0] - x0) < self.__snap_distance and abs(mouse_pos[1] - y0) < self.__snap_distance:
+                        new_vertex_pos = (x0, y0)
+                    elif abs(mouse_pos[0] - x1) < self.__snap_distance and abs(mouse_pos[1] - y0) < self.__snap_distance:
+                        new_vertex_pos = (x1, y0)
+                    elif abs(mouse_pos[0] - x0) < self.__snap_distance and abs(mouse_pos[1] - y1) < self.__snap_distance:
+                        new_vertex_pos = (x0, y1)
+                    elif abs(mouse_pos[0] - x1) < self.__snap_distance and abs(mouse_pos[1] - y1) < self.__snap_distance:
+                        new_vertex_pos = (x1, y1)
+
+        if self.__new_platform_vertex_number == 1:
+            self.__new_platform_first_vertex_pos = new_vertex_pos
+        else:
+            self.__new_platform_second_vertex_pos = new_vertex_pos
+
+        if self._command == Command.CLICKED_LMB:
+            if self.__new_platform_vertex_number == 1:
+                self.__new_platform_vertex_number = 2
+            else:
+                #dodanie nowej platformy
+                x0 = min(self.__new_platform_first_vertex_pos[0], self.__new_platform_second_vertex_pos[0])
+                x1 = max(self.__new_platform_first_vertex_pos[0], self.__new_platform_second_vertex_pos[0])
+                                                                  
+                y0 = min(self.__new_platform_first_vertex_pos[1], self.__new_platform_second_vertex_pos[1])
+                y1 = max(self.__new_platform_first_vertex_pos[1], self.__new_platform_second_vertex_pos[1])
+
+                new_object = GameObject(x0, y0, x1 - x0, y1 - y0, False, ObjectType.STATIC, None)
+
+                self.__game_objects_arr.append(new_object)
+                self.__all_sprites.add(new_object)
+
+                self.__new_platform_first_vertex_pos = self.__new_platform_second_vertex_pos = (-1, -1)
+                self.__new_platform_vertex_number = 1
+        if self._command == Command.CLICKED_RMB:
 
             if self.__new_platform_vertex_number == 1:
-                self.__new_platform_first_vertex_pos = new_vertex_pos
+                self.__new_platform_first_vertex_pos = (-1, -1)
+                self.__mode = EditingMode.NONE
             else:
-                self.__new_platform_second_vertex_pos = new_vertex_pos
+                self.__new_platform_second_vertex_pos = (-1, -1)
+                self.__new_platform_vertex_number = 1
 
-            if self._command == Command.CLICKED_LMB:
-                if self.__new_platform_vertex_number == 1:
-                    self.__new_platform_vertex_number = 2
-                else:
-                    #dodanie nowej platformy
-                    x0 = min(self.__new_platform_first_vertex_pos[0], self.__new_platform_second_vertex_pos[0])
-                    x1 = max(self.__new_platform_first_vertex_pos[0], self.__new_platform_second_vertex_pos[0])
-                                                                      
-                    y0 = min(self.__new_platform_first_vertex_pos[1], self.__new_platform_second_vertex_pos[1])
-                    y1 = max(self.__new_platform_first_vertex_pos[1], self.__new_platform_second_vertex_pos[1])
+    def update_object_placement(self):
+        #TODO zrobić to
+        pass
 
-                    new_object = GameObject(x0, y0, x1 - x0, y1 - y0, False, ObjectType.STATIC, None)
+    def update_deletion(self):
+        obj_to_del_index = -1
+        self.__object_to_delete_coords = (-1, -1, -1, -1)
 
-                    self.__game_objects_arr.append(new_object)
-                    self.__all_sprites.add(new_object)
+        mouse_pos = py.mouse.get_pos()
 
-                    self.__new_platform_first_vertex_pos = self.__new_platform_second_vertex_pos = (-1, -1)
-                    self.__new_platform_vertex_number = 1
-            if self._command == Command.CLICKED_RMB:
+        #wyszukiwanie obietku nad którym znajduje się kursor myszy (potenjalny kandydat do usunięcia)
+        for object in self.__game_objects_arr:
+            if object.get_x() <= mouse_pos[0] and object.get_x() + object.get_width() >= mouse_pos[0] and object.get_y() <= mouse_pos[1] and object.get_y() + object.get_height() >= mouse_pos[1]:
+                
+                #pobranie jego wymiarów (wsp. x, y oraz szerokość i wysokość)
+                self.__object_to_delete_coords = (object.get_x(), object.get_y(), object.get_width(), object.get_height())
+                
+                #zapamiętanie jego indeksu
+                obj_to_del_index = self.__game_objects_arr.index(object)
+                break
 
-                if self.__new_platform_vertex_number == 1:
-                    self.__new_platform_first_vertex_pos = (-1, -1)
-                    self.__mode = EditingMode.NONE
-                else:
-                    self.__new_platform_second_vertex_pos = (-1, -1)
-                    self.__new_platform_vertex_number = 1
-
-        elif self.__mode == EditingMode.OBJECT_PLACEMENT:
-            #TODO zrobić to
-            pass
-        
-        elif self.__mode == EditingMode.DELETION:
-            
-            obj_to_del_index = -1
-            self.__object_to_delete_coords = (-1, -1, -1, -1)
-
-            mouse_pos = py.mouse.get_pos()
-
-            for object in self.__game_objects_arr:
-                if object.get_x() <= mouse_pos[0] and object.get_x() + object.get_width() >= mouse_pos[0] and object.get_y() <= mouse_pos[1] and object.get_y() + object.get_height() >= mouse_pos[1]:
-                    self.__object_to_delete_coords = (object.get_x(), object.get_y(), object.get_width(), object.get_height())
-                    obj_to_del_index = self.__game_objects_arr.index(object)
-                    break
-
-            if self._command == Command.CLICKED_LMB and len(self.__game_objects_arr) > 0 and obj_to_del_index != -1:
-                self.__all_sprites.remove(self.__game_objects_arr.pop(obj_to_del_index))
+        if self._command == Command.CLICKED_LMB and len(self.__game_objects_arr) > 0 and obj_to_del_index != -1:
+            self.__all_sprites.remove(self.__game_objects_arr.pop(obj_to_del_index))
 
     def load_level_from_file(self):
         #odczyt wybranego przez użytkownika pliku
