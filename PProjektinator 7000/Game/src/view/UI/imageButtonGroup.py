a1f324e476592d9bf9c229d80fdc0b14b6150982
Mateusz Chłopek <32665400+OftenDeadKanji@users.noreply.github.com>
2020-05-20 00:26:56 +0200

    ImageButtonGroup działa w dobrych warunkach.

--- PProjektinator 7000/Game/src/view/UI/imageButtonGroup.py
+++ PProjektinator 7000/Game/src/view/UI/imageButtonGroup.py
@@ -22,21 +22,32 @@ class ImageButtonGroup(Control):
         self.__direction = direction
         self.__division = division
         
+        #klikanie slide buttonów - ms
+        self.__cooldown = 500
+        self.__prev_time = 0
+        self.__current_time = 0
+
         #dodać skalowanie
-        extra_size = 50
+        self.__extra_size = 50
 
         #pionowo
         if direction == 0:
-            self._size = (self._size[0] * division[0] + extra_size, self._size[1] * division[1])
+            self._size = (self._size[0] * division[0] + self.__extra_size, self._size[1] * division[1])
         #poziomo
         else:
-            self._size = (self._size[0] * division[0], self._size[1] * division[1] + extra_size)
+            self._size = (self._size[0] * division[0], self._size[1] * division[1] + self.__extra_size)
 
         self._prev_button = Button("", 20, (0, 0), False, Command.PREV_OBJECTS)
         self._next_button = Button("", 20, (0, 0), False, Command.NEXT_OBJECTS)
 
+        self.__init_slide_buttons()
+
+        #0 - bez przesunięcia przycisków w siatce
+        self.__slide_offset = 0
+
+    def __init_slide_buttons(self):
         #pionowo
-        if direction == 0:
+        if self.__direction == 0:
             self._prev_button.set_text("^")
             self._prev_button.set_pos((self._pos[0] + self._size[0], self._pos[1]))
             self._next_button.set_text("v")
@@ -44,26 +55,28 @@ class ImageButtonGroup(Control):
         #poziomo
         else:
             self._prev_button.set_text("<")
-            self._prev_button.set_pos((self._pos[0] + self._size[0] // 4 - self._prev_button.get_size()[0], self._pos[1] + self._size[1] - extra_size))
+            self._prev_button.set_pos((self._pos[0] + self._size[0] // 4 - self._prev_button.get_size()[0], self._pos[1] + self._size[1] - self.__extra_size))
             self._next_button.set_text(">")
-            self._next_button.set_pos((self._pos[0] + 3 * self._size[0] // 4 , self._pos[1] + self._size[1] - extra_size))
-
-        #0 - bez przesunięcia przycisków w siatce
-        self.__slide_offset = 0
+            self._next_button.set_pos((self._pos[0] + 3 * self._size[0] // 4 , self._pos[1] + self._size[1] - self.__extra_size))
 
     #metoda do aktualizowania stanu kontrolki, np.  zmiany koloru
     def update(self):
         self._prev_button.update()
         self._next_button.update()
 
+        self.__current_time = py.time.get_ticks()
+        
+        #TODO sprawdzić czy nie należy zamienić division
         if py.mouse.get_pressed()[0]:
-            if self._prev_button.get_is_focused() and self.__slide_offset > 0:
-                self.__slide_offset -= 1
-            elif self._next_button.get_is_focused():
-               if self.__direction == 0 and self.__slide_offset * self.__division[0] <= len(self.__img_buttons):
-                   self.__slide_offset += 1
-               elif self.__direction == 1 and self.__slide_offset * self.__division[1] <= len(self.__img_buttons):
-                   self.__slide_offset += 1
+            if self.__current_time > self.__prev_time + self.__cooldown:
+                self.__prev_time = self.__current_time
+                if self._prev_button.get_is_focused() and self.__slide_offset > 0:
+                    self.__slide_offset -= 1
+                elif self._next_button.get_is_focused():
+                   if self.__direction == 0 and self.__slide_offset * self.__division[0] + self.__division[0] * self.__division[1] <= len(self.__img_buttons):
+                       self.__slide_offset += 1
+                   elif self.__direction == 1 and self.__slide_offset * self.__division[1] + self.__division[0] * self.__division[1] <= len(self.__img_buttons):
+                       self.__slide_offset += 1
 
         self._isFocused = False
         for i in range(0, self.__division[0] * self.__division[1]):
@@ -81,15 +94,16 @@ class ImageButtonGroup(Control):
                 if self.__img_buttons[i + self.__slide_offset * self.__division[0] - 1].get_is_focused():
                     self._isFocused = True
             else:
-                if i + self.__slide_offset * self.__division[1] - 1 >= len(self.__img_buttons):
+                index = i + self.__slide_offset * self.__division[1] - 1
+                if i + self.__slide_offset * self.__division[1] >= len(self.__img_buttons):
                     break
-                new_x = int(self._pos[0] + self.__cell_size[0] * (i // self.__division[0]))
+                new_x = int(self._pos[0] + self.__cell_size[0] * (i // (self.__division[0] - 1)))
                 new_y = int(self._pos[1] + self.__cell_size[1] * (i % self.__division[1]))
 
-                self.__img_buttons[i + self.__slide_offset * self.__division[1] - 1].set_pos((new_x, new_y))
-                self.__img_buttons[i + self.__slide_offset * self.__division[1] - 1].update()
+                self.__img_buttons[i + self.__slide_offset * self.__division[1]].set_pos((new_x, new_y))
+                self.__img_buttons[i + self.__slide_offset * self.__division[1]].update()
 
-                if self.__img_buttons[i + self.__slide_offset * self.__division[1] - 1].get_is_focused():
+                if self.__img_buttons[i + self.__slide_offset * self.__division[1]].get_is_focused():
                     self.set_is_focused(True)
 
     def set_buttons(self, buttons=[]):
@@ -104,14 +118,14 @@ class ImageButtonGroup(Control):
         for i in range(0, self.__division[0] * self.__division[1]):
             if self.__direction == 0:
                 #break, gdy przekroczy zakres
-                if i + self.__slide_offset * self.__division[0] - 1 >= len(self.__img_buttons):
+                if i + self.__slide_offset * self.__division[0] >= len(self.__img_buttons):
                     break
-                self.__img_buttons[i + self.__slide_offset * self.__division[0] - 1].draw(surface)
+                self.__img_buttons[i + self.__slide_offset * self.__division[0]].draw(surface)
             else:
                 #break, gdy przekroczy zakres
-                if i + self.__slide_offset * self.__division[1] - 1 >= len(self.__img_buttons):
+                if i + self.__slide_offset * self.__division[1] >= len(self.__img_buttons):
                     break
-                self.__img_buttons[i + self.__slide_offset * self.__division[1] - 1].draw(surface)
+                self.__img_buttons[i + self.__slide_offset * self.__division[1]].draw(surface)
 
         for i in range(0, self.__division[0]):
             for j in range(0, self.__division[1]):
